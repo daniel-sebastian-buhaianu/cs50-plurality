7e9443a0a88c259aa8e8afac947995e4083a5dba
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-03-21 06:20:29 +0200

    implemented print_winner function

--- plurality.c
+++ plurality.c
@@ -88,6 +88,20 @@ bool vote(char* name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    // TODO
+	int max_votes = 0;
+	for (int i = 0; i < MAX; i++)
+	{
+		if (candidates[i].votes > max_votes)
+		{
+			max_votes = candidates[i].votes;
+		}
+	}
+	for (int i = 0; i < MAX; i++)
+	{
+		if (candidates[i].votes == max_votes)
+		{
+			printf("%s\n", candidates[i].name);
+		}
+	}
     return;
 }
